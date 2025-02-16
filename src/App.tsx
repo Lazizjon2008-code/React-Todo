f5aedd22b1858ad90f08cad29ea23fb0b56ebcd7
rameo2012 <rustambekovramziddin5@gmail.com>
2025-02-16 16:14:56 +0300

    write a style css

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+function App() {
+    
+  return (
+    <>
+    <input className="input" type="text" />
+    <button className="submit">Add</button>
+    <div className="main_box">
+      <div className="Box">
+              <button className="removebtn">Remove</button>
+              <button className="addbtn">Done</button>
+          </div>
+    </div>
+    </>
+  )
+}
+
+
+export default App
