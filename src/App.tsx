35a9757dfb61fd994b8a4967f8cb8d8605640d64
Lazizjon2008-code <laziizjon04@gmail.com>
2025-02-22 21:21:03 +0500

    added cookie

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+function setCookie(name: string, value: string) {
+  const expires = new Date();
+  expires.setMonth(expires.getMonth() + 1); 
+  document.cookie = `${name}=${value}; expires=${expires.toUTCString()}; path=/`;
+}
+
+function getCookie(name: string) {
+  const value: string = `; ${document.cookie}`;
+  const parts: string[] = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    return parts.pop()!.split(";").shift();
+  } 
+  return null;
+}
+
+
+interface Todo {
+  id: number;
+  tittle: string;
+  done: boolean;
+}
 
 function App() {
 
-  interface Todo {
-    id: number;
-    tittle: string;
-    done: boolean;
-  }
 
   const [todos, setTodos] = useState<Todo[]>([])
   const [input, setInput] = useState<string>("")
+
+
+  useEffect(  () => {
+    const cookieTodos = getCookie("todos")
+    if(cookieTodos){
+      setTodos(JSON.parse(cookieTodos))
+    }
+  },[])
+
+  useEffect(() => {
+    setCookie("todos", JSON.stringify(todos))
+  }, [todos])
+
 
   function addTodo() {
     if(!input.trim()) return;
@@ -36,6 +66,7 @@ function App() {
   
   return (
     <>
+    <div className="container">
     <input onChange={(e) => setInput(e.target.value)} className="input" value={input} type="text" />
     <button className="add_btn" onClick={addTodo}>Add</button>
    
@@ -49,6 +80,7 @@ function App() {
           </div>
         ))
       }
+    </div>
     </div>
     </>
   )
